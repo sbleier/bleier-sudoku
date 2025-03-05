cd5672e00c22db54b7ddbd7ca635deb4c8fa1c08
SARA BLEIER <sbleier2@student.touro.edu>
2025-03-04 23:56:38 -0500

    Project won't build

---
+++ src/main/java/bleier/sudoku/Sudoku.java
@@ -0,0 +1,65 @@
+package bleier.sudoku;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Sudoku {
+    int[][] board;
+
+    Sudoku(int[][] board) {
+        this.board = board;
+    }
+
+    public List<String> getErrors() {
+        List<String> errors = new ArrayList<>();
+        checkColumns(errors);
+        checkRows(errors);
+        checkBoxes(errors);
+        return errors;
+    }
+
+    public void checkRows(List<String> errors) {
+        for (int r = 0; r < 9; r++) {
+            HashMap<Integer, Integer>  hashmap = new HashMap<>();
+            for (int c = 0; c < 9; c++) {
+                int num = board[r][c];
+                if (hashmap.containsKey(num)) {
+                    errors.add("Row " + r + ", Column " + c + " contains duplicate value: " + num);
+                }
+                hashmap.put(num, hashmap.getOrDefault(num, 0) + 1);
+            }
+        }
+    }
+
+    public void checkColumns(List<String> errors) {
+        for (int c = 0; c < 9; c++) {
+            HashMap<Integer, Integer>  hashmap = new HashMap<>();
+            for (int r = 0; r < 9; r++) {
+                int num = board[r][c];
+                if (hashmap.containsKey(num)) {
+                    errors.add("Row " + r + ", Column, " + c + " contains duplicate value: " + num);
+                }
+                hashmap.put(num, hashmap.getOrDefault(num, 0) + 1);
+            }
+        }
+    }
+
+    public void checkBoxes(List<String> errors) {
+        int boxCount = 1;
+        for(int bRow = 0; bRow < 3; bRow++) {
+            for(int bCol = 0; bCol < 3; bCol++) {
+                HashMap<Integer, Integer>  hashmap = new HashMap<>();
+                for (int r = bRow * 3; r < r+3; r++) {
+                    for (int c = bCol * 3; c < c+3; c++) {
+                        int num = board[r][c];
+                        if (hashmap.containsKey(num)) {
+                            errors.add("Box " + boxCount + ", Row " + r + ", Column " + c + " contains duplicate value: " + num);
+                        }
+                    }
+                }
+                boxCount++;
+            }
+        }
+    }
+}
---
+++ src/main/java/bleier/sudoku/SudokuTest.java
@@ -0,0 +1,30 @@
+package bleier.sudoku;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+public class SudokuTest {
+
+    @Test
+    public void noErrors() {
+        int[][] sudokuBoard = {
+                {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                {3, 4, 5, 2, 8, 6, 1, 7, 9}
+        };
+
+        Sudoku sudoku = new Sudoku(sudokuBoard);
+        List<String> errors = sudoku.getErrors();
+
+        assertTrue(errors.isEmpty());
+
+    }
+
+}
